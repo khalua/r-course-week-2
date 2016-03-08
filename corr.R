1bdbd4b89a0db5ff85bff235a5dd2b6c1c1d0627
Tony Contreras <tony.contreras@gmail.com>
2016-03-08 10:36:07 -0800

    done with part 3. Holy shit that was complicated.

--- corr.R
+++ corr.R
@@ -1,16 +1,21 @@
 ## part 3 of 3, week 2 programming R assignment, HOLY SHIT THIS IS HARD
 
-corr <- function(threshold = 0) {
-##        data.files <- list.files(path = datadir, pattern = "csv$", full.names = TRUE)
-        all.data <- data.frame()   ## all.data gets cleaned at every loop
-##       all.data <- rbind(all.data, read.csv(data.files) ##cause this fucker likes to append.
-        all.data <- read.csv("specdata/001.csv")
-        good <- complete.cases(all.data) ## temp variable to get to next one
-        all.data.minus.na <- all.data[good,]
-        if (length(all.data.minus.na$Date) >= threshold) {
-                print("threshold ok")
-        } else {
-                print("threshold too low")
-        }
+
+corr <- function(datadir, threshold = 0) {
+        data.files <- list.files(path = datadir, pattern = "csv$", full.names = TRUE)
+        cor.v <- numeric() # initialize correlation vector 
         
+        for (i in data.files) {
+                all.data <- data.frame() # resets all.data data frame
+                good <- logical() ## resets good
+                all.data.minus.na <- data.frame()
+                all.data <- rbind(all.data, read.csv(i)) ## read all the files
+                good <- complete.cases(all.data) ## temp variable to get to next one
+                all.data.minus.na <- all.data[good,]
+                if (length(all.data.minus.na$Date) > threshold) {
+                        cor.result <- cor(all.data.minus.na$sulfate, all.data.minus.na$nitrate)
+                        cor.v <- append(cor.v, cor.result)
+                }
+        }
+        cor.v
 }
